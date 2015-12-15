bf5baf7bbd9913aadc4ffa807bac99317f5c50a2
Mykhailo.Romaniuk <mishani0x0ef@gmail.com>
2015-12-15 23:56:43 +0000

    Added validation to textboxes.

--- MvvmDemo/MvvmDemo/View/MainView.xaml.cs
+++ MvvmDemo/MvvmDemo/View/MainView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace MvvmDemo.View
 {
@@ -9,6 +10,20 @@ namespace MvvmDemo.View
         public MainView()
         {
             InitializeComponent();
+        }
+
+        // Обмежуємо допустимий ввід в наші текстові поля тільки цифрами.
+        // Обробка подій натискання кнопки не відповідає практикам MVVM. 
+        // Є кращі шляхи реалізації такого обмеження, але вони б ускладнили цей приклад. Тому для розуміння зроблено так.
+        private void UIElement_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            var isNumeric = e.Key >= Key.D0 && e.Key <= Key.D9;
+            isNumeric = isNumeric || (e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9);
+            var isControl = e.Key == Key.Delete || e.Key == Key.Back;
+            if (!isNumeric && !isControl)
+            {
+                e.Handled = true;
+            }
         }
     }
 }
