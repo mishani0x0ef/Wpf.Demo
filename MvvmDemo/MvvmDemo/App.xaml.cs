ec2ddf61e83ba280d452b9c14954ee1c809352c0
Mykhailo.Romaniuk <mishani0x0ef@gmail.com>
2015-12-15 22:39:25 +0000

    Implemented view representation.

--- MvvmDemo/MvvmDemo/App.xaml.cs
+++ MvvmDemo/MvvmDemo/App.xaml.cs
@@ -1,9 +1,20 @@
 ﻿using System.Windows;
+using MvvmDemo.View;
+using MvvmDemo.ViewModel;
 
 namespace MvvmDemo
 {
     // Головний клас всієї програми. Місце, де ваша програма стартує.
     public partial class App : Application
     {
+        // Обробник події запуску програми.
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Створюємо View, присвоюємо їй контекст даних (місце звідки бартимуться дані для байндінга) і показуємо.
+            var mainView = new MainView {DataContext = new MainViewModel()};
+            mainView.Show();
+        }
     }
 }
--- MvvmDemo/MvvmDemo/ViewModel/MainViewModel.cs
+++ MvvmDemo/MvvmDemo/ViewModel/MainViewModel.cs
@@ -2,7 +2,8 @@
 
 namespace MvvmDemo.ViewModel
 {
-    // View Model нашої MainView
+    // View Model нашої MainView. Вона наслідується від класа ViewModelBase з ліби GalaSoft.MvvmLight, яку я підключив в менеджері пакетів.
+    // Базовий клас ViewModelBase надає нам всі потрібні засоби для реалізації байндінгу даних і команд.
     public class MainViewModel : ViewModelBase
     {
         public MainViewModel()
